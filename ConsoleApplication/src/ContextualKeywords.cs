11538f870b6d6e3cff10880f8c38e136121e532a
EduardoSantosGit <eduardosantos058@gmail.com>
2017-08-06 20:55:29 -0300

    implement class partial

---
+++ ConsoleApplication/src/ContextualKeywords.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApplication
+{
+    public partial class ContextualKeywords
+    {
+        public int NumberReturnClass1()
+        {
+            return 10;
+        }
+
+    }
+    
+    public partial class ContextualKeywords
+    {
+        public int NumberReturnClass2()
+        {
+           return 20;
+        }
+    }
+}
---
+++ ProjectTestExamples/src/ContextualKeywordsTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using ConsoleApplication;
+
+namespace ProjectTestExamples
+{
+    public class ContextualKeywordsTest
+    {
+        [Fact]
+        public void NumberReturnClass1_When_Return10()
+        {   
+            var c = new ContextualKeywords();
+            Assert.Equal(c.NumberReturnClass1(),10);
+        }
+        
+        [Fact]
+        public void NumberReturnClass2_When_Return10()
+        {   
+            var c = new ContextualKeywords();
+            Assert.Equal(c.NumberReturnClass2(),20);
+        }
+    }
+}
